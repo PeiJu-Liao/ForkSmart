141d69bfd5c67031200eff1a2f574c46146cb314
PeiJu-Liao <peggy1996850519@gmail.com>
2022-08-24 16:58:53 +0800

    資料Session存取/撈取

--- config/passport.js
+++ config/passport.js
@@ -36,3 +36,13 @@ passport.use(new LocalStrategy({
 }
 ))
 
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
