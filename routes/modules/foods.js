866b3bb1d641bdec10eced6403683688337e6caa
PeiJu-Liao <peggy1996850519@gmail.com>
2022-08-25 15:26:11 +0800

    food.html

--- routes/modules/foods.js
+++ routes/modules/foods.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('foods')
+  res.render('food')
 })
 
 router.get('/:id', (req, res) => {
