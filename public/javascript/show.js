16a903975d958c2bc0f51f84f3f33162a7bd2966
PeiJu-Liao <peggy1996850519@gmail.com>
2022-08-28 16:09:49 +0800

    progress bar add

--- public/javascript/show.js
+++ public/javascript/show.js
@@ -1,1 +1,8 @@
 console.log(`Here is show.js`)
+
+const progressBar = document.querySelectorAll('.progressbar')
+
+progressBar.forEach(bar => {
+  bar.style.width = bar.getAttribute('data-done') + '%'
+  bar.style.opacity = 1
+})
