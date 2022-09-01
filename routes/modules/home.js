92fd11a9a10ca2f7251bd3cda38c325b37044835
PeiJu-Liao <peggy1996850519@gmail.com>
2022-09-01 16:53:19 +0800

    rwd revised

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const forksmartData = require('../../forksmart.json')
 
 // Route
 router.get('/', (req, res) => {
   res.render('index', {
     title: 'Fork Smart',
+    forksmartData: forksmartData.results
   });
 })
 
