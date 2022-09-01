99a88bd5b4e60eeca483b9fad999f693aef65370
PeiJu-Liao <peggy1996850519@gmail.com>
2022-09-01 14:42:52 +0800

    show style/data loop

--- routes/modules/foods.js
+++ routes/modules/foods.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const router = express.Router()
+// json file loading
+const forksmartData = require('../../forksmart.json')
 
 router.get('/', (req, res) => {
-  res.render('food')
+  res.render('food', {
+    forksmartData: forksmartData.results
+  })
 })
 
 router.get('/:id', (req, res) => {
-  res.render('show', {
-    script: 'show.js'
+  // find()只會回傳第一次為true的值
+  const forksmartFood = forksmartData.results.find(item => {
+    return item.id.toString() === req.params.id
   })
+  res.render('show', { forksmartFood })
 })
 
 module.exports = router
