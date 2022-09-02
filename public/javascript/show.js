b648b0961a726e41c0fd496c7384441732e6aac7
PeiJu-Liao <peggy1996850519@gmail.com>
2022-09-02 13:40:40 +0800

    vitamin modal toggle js

--- public/javascript/show.js
+++ public/javascript/show.js
@@ -1,6 +1,18 @@
 const progressBar = document.querySelectorAll('.progressbar')
+const vitaminToggle = document.querySelectorAll('.show__vitamin-name')
+const tabLink = document.querySelectorAll('.nav-link')
 
 progressBar.forEach(bar => {
   bar.style.width = bar.getAttribute('data-done') + '%'
   bar.style.opacity = 1
+})
+
+vitaminToggle.forEach(vit => {
+  vit.addEventListener('click', () => {
+    tabLink.forEach(link => {
+      if (link.getAttribute('data-bs-target') === vit.getAttribute('data-name')) {
+        link.click()
+      }
+    })
+  })
 })
