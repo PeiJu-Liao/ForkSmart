e1cdd977ea43a768ed237558be22e49ddd9a0fd9
PeiJu-Liao <peggy1996850519@gmail.com>
2022-09-07 00:28:50 +0800

    feat: search function add

--- routes/modules/foods.js
+++ routes/modules/foods.js
@@ -2,18 +2,50 @@ const express = require('express')
 const router = express.Router()
 // json file loading
 const forksmartData = require('../../forksmart.json')
+const pagination = {}
 
-router.get('/', (req, res) => {
-  // TODO: 分頁功能: http://localhost:3000/foods?page=1&limit=12
+router.get('/', paginationControl(forksmartData.results), (req, res) => {
+
   res.render('food', {
-    forksmartData: forksmartData.results,
+    forksmartData: pagination.results,
     isResultExist: true,
   })
 })
 
-router.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim()
+router.get('/search', paginationControl(forksmartData.results), (req, res) => {
+  const keyword = req.query.keyword.trim() || ""
   console.log(`req.query:`, req.query)
+  const researchResults = forksmartData.results.filter(item => {
+    return item.name_en.toLowerCase().includes(keyword.toLowerCase()) || item.name.trim().includes(keyword) || item.category.trim().includes(keyword)
+  })
+
+  // FIXME: 之後分頁功能要修改(CodeShit)
+  if (keyword === "") {
+    res.redirect('/foods?page=1&limit=8')
+  }
+
+  res.render('food', {
+    forksmartData: researchResults,
+    isResultExist: researchResults.length,
+    keyword
+  })
+})
+
+router.get('/search1', (req, res) => {
+  const category = req.query.category
+  const sortResults = forksmartData.results.filter(item => {
+    return item.category.trim() === category
+  })
+
+  res.render('food', {
+    forksmartData: sortResults,
+    isResultExist: true,
+    category
+  })
+})
+
+router.get('/search2', (req, res) => {
+  const keyword = req.query.keyword.trim()
   const researchResults = forksmartData.results.filter(item => {
     return item.name_en.toLowerCase().includes(keyword.toLowerCase()) || item.name.trim().includes(keyword) || item.category.trim().includes(keyword)
   })
@@ -35,5 +67,34 @@ router.get('/:id', (req, res) => {
     mineral: forksmartData.mineral
   })
 })
+
+function paginationControl(model) {
+  return (req, res, next) => {
+    // TODO: 分頁功能: http://localhost:3000/foods?page=1&limit=12
+    const page = parseInt(req.query.page) //turn it to Integer Number
+    const limit = parseInt(req.query.limit)
+    const startIndex = (page - 1) * limit
+    const endIndex = (page) * limit
+    // slice()回傳一個新陣列
+
+
+    if (endIndex < model.length) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      }
+    }
+    // NOTE: 表示"並非"從第一頁開始
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit
+      }
+    }
+    pagination.results = model.slice(startIndex,
+      endIndex)
+    next()
+  }
+}
 
 module.exports = router
