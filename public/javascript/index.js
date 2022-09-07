eafaa2681e25f832f81e280e652e01ad6ec7080e
PeiJu-Liao <peggy1996850519@gmail.com>
2022-09-07 15:39:47 +0800

    feat: swiper add

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,1 +1,32 @@
-console.log(`Hello World!!`)
+const swiper = new Swiper('.foodswiper', {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  effect: "slide",
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 16,
+    }
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
